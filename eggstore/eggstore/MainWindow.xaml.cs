1ff5e24dcdccfbfb5cefab121791992aaeba2035
Jeury Pierre Dide <elyruej.102004@gmail.com>
2023-03-08 16:29:38 -0400

    2/15 completado
    Creando vista usuario y conectandola con la ventana principal

--- eggstore/eggstore/MainWindow.xaml.cs
+++ eggstore/eggstore/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using eggstore.Views;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace eggstore
 {
@@ -48,6 +37,11 @@ namespace eggstore
         private void Cerrar(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void Usuarios_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new Usuarios();
         }
     }
 }
